9cc1550e999277f6695dd90ac9d4312bd9fb7585
Diego Ortiz <ortizv.da@gmail.com>
2023-06-01 17:08:58 -0500

    login con estilo en prueba.php, ya loguea

---
+++ login-val/db.php
@@ -0,0 +1,16 @@
+<?php
+function connect_db(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "devuelvepe";
+
+
+    $db = new mysqli($servername, $username, $password, $dbname);
+
+    if($db -> connect_error){
+        die("Connection failed: ". $db->connect_error);
+    }
+    return $db;
+}
+?>
