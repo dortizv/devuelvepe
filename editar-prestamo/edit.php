26d117f6b099ba96ea56970aa0064673be1db47e
GaOmPS <omarpintado98@gmail.com>
2023-06-08 19:31:37 -0500

    edit button is already functional

--- editar-prestamo/edit.php
+++ editar-prestamo/edit.php
@@ -1,96 +1,30 @@
 <?php
-    // VALIDACIÓN DE USUARIO LOGUEADO
-    session_start();
-    if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario'] ){
-        
-        $idPrestamo = $_POST['id_prestamo'];
-        include_once("./../login-val/db.php");
+include_once("./../register-val/db.php");
 
-        $userId = $_SESSION['idUsuario'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //EXTRAER VALORES PARA EDITAR PRÉSTAMO
+    $idPrestamo = intval($_POST['idPrestamoEdit']);
+    $clienteEdit = $_POST['clienteEdit'];
+    $cobradorEdit = $_POST['cobradorEdit'];
+    $montoEdit = intval($_POST['montoEdit']);
+    $tasaEdit = intval($_POST['tasaEdit']);
+    $cuotasEdit = intval($_POST['cuotasEdit']);
 
-        $db = connect_db();
+    // INSERTAR LA INFORMACIÓN EN LA BASE DE DATOS
 
-        $sql = "SELECT prestamo.idCliente, cliente.nombre, cliente.apellido, cliente.tipodocumento, cliente.documento 
-                FROM cliente 
-                INNER JOIN prestamo ON prestamo.idCliente = cliente.id
-                INNER JOIN cobrador ON cobrador.id = prestamo.idCobrador
-                WHERE cobrador.idUsuario = ?";
+    $db = connect_db();
 
+    // Utilizando consultas preparadas con mysqli
+    $stmt = $db->prepare("UPDATE prestamo SET monto = ?, tasa = ?, cuotas = ? WHERE id = ?");
+    $stmt->bind_param("iiii", $montoEdit, $tasaEdit, $cuotasEdit, $idPrestamo);
+    $stmt->execute();
 
-        $stmt = $db->prepare($sql);
-        $stmt->bind_param("i", $userId);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        
-        //$row = $result -> fetch_assoc();
-        //var_dump($row);
-        //die();
-?>
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Editar préstamo</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-        <!-- Vendor CSS Files -->
-        <link href="./assets/vendor/aos/aos.css" rel="stylesheet">
-        <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="./assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="./assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-        <link href="./assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="./assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-        <link href="./assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-        <!-- Template Main CSS File -->
-        <link href="./assets/css/style.css" rel="stylesheet">
-        <link href="./assets/css/popups.css" rel="stylesheet">
-        <script src="./assets/js/popup.js"></script>
-
-    </head>
-    <body style="min-width: 560px">
-        <h3>Datos para editar préstamo</h3>
-            <form class="col" action="register-val/lg-register.php" method = "POST">
-            <div class="col-md-4 mb-3">
-                <input type="text" class="form-control" name="distrito" placeholder="Distrito" value="<?php echo $idPrestamo; ?>" required>
-            </div>
-            <div class="col-md-4 mb-3">
-                        <input type="text" class="form-control" name="distrito" placeholder="Distrito" required>
-            </div>
-            <div class="col-md-4 mb-3">
-                        <input type="text" class="form-control" name="distrito" placeholder="Distrito" required>
-            </div>
-            <div class="col-md-4 mb-3">
-                        <input type="text" class="form-control" name="distrito" placeholder="Distrito" required>
-            </div>
-            <div class="row justify-content-center pb-4">
-                    <button class="mx-3" style="width: fit-content" type="submit">Guardar</button>
-                    <button class="mx-3" style="background-color: lightgray; width: fit-content" type="submit" formaction="./../prestamos.php" formnovalidate>Cancelar</button>
-            </div>
-        </form>
-
-        <!-- Vendor JS Files -->
-        <script src="./assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="./assets/vendor/aos/aos.js"></script>
-        <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="./assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="./assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="./assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-        <!-- Template Main JS File -->
-        <script src="./assets/js/main.js"></script>
-
-    </body>
-</html>
-
-    <?php
-//FIN DE VALIDACIÓN USUARIO LOGUEADO
-}else{
-    header("Location:./login.php");
-    exit;
+    // Comprueba si la consulta se ejecutó correctamente
+    if ($stmt->affected_rows > 0) {
+        echo "El préstamo se ha actualizado exitosamente.";
+        header("Location: ./../prestamos.php");
+    } else {
+        echo "No se encontró el préstamo con el ID especificado.";
+    }
 }
 ?>
--- prestamos.php
+++ prestamos.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
 
     $db = connect_db();
 
-    $sql = "SELECT prestamo.idCliente, cliente.nombre AS cliente_nombre, cliente.apellido AS cliente_apellido, cobrador.nombre AS cobrador_nombre, cobrador.apellido AS cobrador_apellido, prestamo.monto, prestamo.cuotas, prestamo.tasa, prestamo.fecha 
+    $sql = "SELECT prestamo.id AS id_prestamo, cliente.nombre AS cliente_nombre, cliente.apellido AS cliente_apellido, cobrador.nombre AS cobrador_nombre, cobrador.apellido AS cobrador_apellido, prestamo.monto, prestamo.cuotas, prestamo.tasa, prestamo.fecha 
                 FROM prestamo 
                 INNER JOIN cobrador ON cobrador.id = prestamo.idCobrador
                 INNER JOIN usuario ON usuario.id=cobrador.idUsuario 
@@ -22,9 +22,30 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
     $stmt->execute();
     $result = $stmt->get_result();
 
-    //$row = $result -> fetch_assoc();
-    //var_dump($row);
-    //die();
+
+    //<!--Consulta a la BD para extraer listado de clientes y cobradores -->
+    $opciones_clientes = "SELECT cliente.id, cliente.nombre, cliente.apellido 
+                        FROM cliente
+                        ORDER BY cliente.nombre ASC";
+    $result_clientes = $db->query($opciones_clientes);
+
+    $opciones_cobrador = "SELECT cobrador.id, cobrador.nombre, cobrador.apellido
+                                FROM cobrador
+                                WHERE idUsuario = $userId
+                                ORDER BY cobrador.nombre ASC";
+    $result_cobradores = $db->query($opciones_cobrador);
+
+    $list_clientes = '';
+    $list_cobradores = '';
+
+    while ($row = $result_clientes->fetch_assoc()) {
+        $list_clientes .= "<option value='" . $row['id'] . "'>" . $row['nombre'] . ' ' . $row['apellido'] . "</option>";
+    }
+
+    while ($row = $result_cobradores->fetch_assoc()) {
+        $list_cobradores .= "<option value='" . $row['id'] . "'>" . $row['nombre'] . ' ' . $row['apellido'] . "</option>";
+    }
+    //<!-- Fin de consulta a la BD para listado de clientes y cobradores  -->
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -68,32 +89,8 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
             </div>
 
             <p style="margin-top: ; font-weight: 600; font-family: Raleway; font-size: 28px; text-align: center">Nuevo préstamo</p>
+            
             <div class="container">
-                <!-- CONSULTAS A LA BASE DE DATOS PARA EXTRAER LISTADO DE CLIENTES Y COBRADORES -->
-                <?php
-                $opciones_clientes = "SELECT cliente.id, cliente.nombre, cliente.apellido 
-                                            FROM cliente
-                                            ORDER BY cliente.nombre ASC";
-                $result_clientes = $db->query($opciones_clientes);
-
-                $opciones_cobrador = "SELECT cobrador.id, cobrador.nombre, cobrador.apellido
-                                          FROM cobrador
-                                          WHERE idUsuario = $userId
-                                          ORDER BY cobrador.nombre ASC";
-                $result_cobradores = $db->query($opciones_cobrador);
-
-                $list_clientes = '';
-                $list_cobradores = '';
-
-                while ($row = $result_clientes->fetch_assoc()) {
-                    $list_clientes .= "<option value='" . $row['id'] . "'>" . $row['nombre'] . ' ' . $row['apellido'] . "</option>";
-                }
-
-                while ($row = $result_cobradores->fetch_assoc()) {
-                    $list_cobradores .= "<option value='" . $row['id'] . "'>" . $row['nombre'] . ' ' . $row['apellido'] . "</option>";
-                }
-                ?>
-
                 <div class="mb-3">
                     <label for="cliente" style="font-family: Raleway; font-weight: 600; font-size: 16px">Selecciona un cliente:</label>
                     <select class="form-control" id="cliente" name="cliente" style="font-family: Raleway; font-weight: 600; font-size: 16px">
@@ -107,7 +104,6 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
                         <?php echo $list_cobradores; ?>
                     </select>
                 </div>
-                <!-- FIN DE CONSULTA A BD PARA LISTA DE CLIENTE Y COBRADOR  -->
 
                 <div class="mb-3">
                     <label for="cobrador" style="font-family: Raleway; font-weight: 600; font-size: 16px">Ingresa el monto (S/):</label>
@@ -126,8 +122,6 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
 
                 <button onclick="closeDiv()" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #2A9D8F; color: white; font-family: Raleway; font-weight: 600; font-size: 14px" type="submit">Aceptar</button>
             </div>
-
-
         </div>
     </div>
     <!-- FIN NUEVO PRÉSTAMO -->
@@ -140,50 +134,54 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
                 <g><path d="M498.9,615.4c-2.5,3.4-3.7,5.6-5.4,7.3c-113.6,113.7-227.2,227.3-340.9,341c-24.4,24.4-53.5,32.4-86.4,21.7c-58-18.9-75.3-92.4-32-136C95.1,788,156.5,727,217.7,665.8c53.3-53.3,106.6-106.6,159.9-159.9c1.6-1.6,3-3.2,6-6.4c-2.7-1.9-5.5-3.4-7.6-5.5c-112.4-112.3-224.8-224.7-337.1-337C7.2,125.3,3.7,79.4,30.2,46.1c30.7-38.5,86.1-41.8,121.9-6.9c26.1,25.5,51.7,51.5,77.5,77.3c87.9,87.8,175.7,175.7,263.5,263.6c1.6,1.6,2.5,3.7,4,5.9c2.9-2.8,4.5-4.3,6.1-5.8C618.3,265.1,733.4,150,848.5,35C888.5-4.9,953.1,4.1,980,53.1c17.6,32,11.8,71.2-14.8,98c-31.4,31.6-63,63.1-94.6,94.6c-83.7,83.7-167.4,167.4-251.1,251.1c-1.4,1.4-2.8,2.9-5.8,6.1c4.4,3.7,9.1,7,13,10.9c111,110.9,221.9,221.8,332.8,332.8c28.6,28.6,33.4,69.4,12.7,102.5c-27.9,44.6-89.3,51.5-127.2,13.9c-47.6-47.2-94.9-94.8-142.4-142.3c-65.9-65.9-131.8-131.8-197.7-197.8C503.2,621.2,501.8,619.1,498.9,615.4z"/></g>
                 </svg>
             </div>
-
+        
             <p style="margin-top: ; font-weight: 600; font-family: Raleway; font-size: 28px; text-align: center">Editar préstamo</p>
             <div class="container">
                 
+                <form action="./editar-prestamo/edit.php" method = "POST">
+                
+                <!-- campo id -->
+                    <div class="inputId">
+                        <input class = ""type="number" readonly="readonly" class="form-control" id="idPrestamoEdit" name="idPrestamoEdit" style = "float: right; text-align: right; border: 1px solid whitesmoke;color:whitesmoke; background-color: whitesmoke; width:10px;">
+                </div>
+    
                 <!-- campo cliente -->
                 <div class="mb-3">
-                    <label for="cliente" style="font-family: Raleway; font-weight: 600; font-size: 16px">Cliente:</label>
-                    <select class="form-control" id="cliente" name="cliente" style="font-family: Raleway; font-weight: 600; font-size: 16px">
-                        <?php echo $list_clientes; ?>
-                    </select>
+                    <label for="clienteEdit" style="font-family: Raleway; font-weight: 600; font-size: 16px">Cliente:</label>
+                    <input type="text" readonly class="form-control" id="clienteEdit" name="clienteEdit" required style="background-color: whitesmoke">
                 </div>
                 
                 <!-- campo cobrador -->
                 <div class="mb-3">
-                    <label for="cobrador" style="font-family: Raleway; font-weight: 600; font-size: 16px">Cobrador:</label>
-                    <select class="form-control" id="cobrador" name="cobrador" style="font-family: Raleway; font-weight: 600; font-size: 16px">
-                        <?php echo $list_cobradores; ?>
-                    </select>
+                    <label for="cobradorEdit" style="font-family: Raleway; font-weight: 600; font-size: 16px">Cobrador:</label>
+                    <input type="text" readonly class="form-control" id="cobradorEdit" name="cobradorEdit" required style="background-color: whitesmoke">
                 </div>
                 
                 <!-- campo monto -->
                 <div class="mb-3">
-                    <label for="monto" style="font-family: Raleway; font-weight: 600; font-size: 16px">Monto (S/):</label>
-                    <input type="text" class="form-control" id="monto" name="monto" style="font-family: Raleway; font-weight: 600; font-size: 16px">
+                    <label for="montoEdit" style="font-family: Raleway; font-weight: 600; font-size: 16px">Monto (S/):</label>
+                    <input type="number" class="form-control" id="montoEdit" name="montoEdit" required>
                 </div>
                 
                 <!-- campo tasa -->
                 <div class="mb-3">
-                    <label for="tasa" style="font-family: Raleway; font-weight: 600; font-size: 16px">Tasa (%):</label>
-                    <input type="text" class="form-control" id="tasa" name="tasa" style="font-family: Raleway; font-weight: 600; font-size: 16px">
+                    <label for="tasaEdit" style="font-family: Raleway; font-weight: 600; font-size: 16px">Tasa (%):</label>
+                    <input type="number" class="form-control" id="tasaEdit" name="tasaEdit" required>
                 </div>
 
                 <!-- campo cuotas -->
                 <div class="mb-3">
-                    <label for="cuotas" style="font-family: Raleway; font-weight: 600; font-size: 16px">Número de cuotas:</label>
-                    <input type="text" class="form-control" id="cuotas" name="cuotas" style="font-family: Raleway; font-weight: 600; font-size: 16px">
+                    <label for="cuotasEdit" style="font-family: Raleway; font-weight: 600; font-size: 16px">Número de cuotas:</label>
+                    <input type="number" class="form-control" id="cuotasEdit" name="cuotasEdit" required>
                 </div>
 
-                <button onclick="closeDiv()" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #2A9D8F; color: white; font-family: Raleway; font-weight: 600; font-size: 14px" type="submit">Aceptar</button>
-            </div>
-    <!-- FIN DESPLEGAR EDITAR PRÉSTAMO -->            
-
+                <button type="submit" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #2A9D8F; color: white; font-family: Raleway; font-weight: 600; font-size: 14px">Aceptar</button>
+                <button type="button" onclick = "closeDiv()" formnovalidate>Cancelar</button>
+                </form>
+            </div>           
         </div>
     </div>
+    <!-- FIN DESPLEGAR EDITAR PRÉSTAMO --> 
 
     <!--DESPLEGABLE AMORTIZAR-->
     <div class="struct" id="amortizar">
@@ -197,7 +195,7 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
             <p style="margin-top: ; font-weight: 600; font-family: Raleway; font-size: 28px; text-align: center">Amortizar</p>
             <div class="container">
                 
-                 <!-- campo cuotas -->
+                 <!-- campo fecha -->
                 <div class="mb-3">
                     <label for="fecha" style="font-family: Raleway; font-weight: 600; font-size: 16px">Fecha</label>
                     <input type="text" class="form-control" id="fecha" name="fecha" style="font-family: Raleway; font-weight: 600; font-size: 16px">
@@ -259,21 +257,33 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
                 <tbody class="align-middle justify-content-center">
                 <?php
                 while ($row = $result -> fetch_assoc()){
+                    //Almacenar valores de RESULT
+                    $idPrestamo = $row['id_prestamo'];
+                    $clienteNA =  $row['cliente_nombre'] .' '.$row['cliente_apellido'];
+                    $cobranorNA = $row['cobrador_nombre'] .' '.$row['cobrador_apellido'];
+                    $cuotas = $row['cuotas'];
+                    $monto = $row['monto'];
+                    $fecha = $row['fecha'];
+                    $tasa = $row['tasa'];
+
+                    
+                    //Mostrar filas
                     echo "<tr class='text-center' style='background-color: #FBF4EE; border: transparent;font-family: Roboto;font-weight: 600; font-size: 15px'>";
-                    echo "<td>" . $row['cliente_nombre'] .' '.$row['cliente_apellido']."</td>";
-                    echo "<td>" . $row['cuotas'] . "</td>";
-                    echo "<td>" . $row['monto'] . "</td>";
-                    echo "<td>" . $row['fecha'] . "</td>";
-                    echo "<td>" . $row['tasa'] . "</td>";
+                    echo "<td>" . $clienteNA."</td>";
+                    echo "<td>" . $cuotas . "</td>";
+                    echo "<td>" . $monto . "</td>";
+                    echo "<td>" . $fecha . "</td>";
+                    echo "<td>" . $tasa . "</td>";
+                  
                     echo '<td class="contact">
-                            <div class="php-email-form">
-                                <button onclick="openDiveditar()" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #E9C46A; color: black; font-family: Raleway; font-weight: 600; font-size: 14px" type="submit">Editar</button>
-                                <button onclick="openDivamor()" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #2A9D8F; color: black; font-family: Raleway; font-weight: 600; font-size: 14px" type="submit">Amortizar</button>
-                            </div>
-                          </td>';
+                    <div class="php-email-form">
+                        <button type="submit" onclick="openDiveditar(\'' . $idPrestamo . '\', \'' . $clienteNA . '\', \'' . $cobranorNA . '\', \'' . $monto . '\', \'' . $tasa . '\', \'' . $cuotas . '\')" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #E9C46A; color: black; font-family: Raleway; font-weight: 600; font-size: 14px">Editar</button>
+                        <button type="submit" onclick="openDivamor()" class="mx-1 my-1" style="width: fit-content; padding: 5px 10px; border-radius: 5px; background-color: #2A9D8F; color: black; font-family: Raleway; font-weight: 600; font-size: 14px">Amortizar</button>
+                    </div>
+                    </td>';
+
                     echo "</tr>";
                 }
-
                 ?>
                 </tbody>
             </table>
@@ -290,6 +300,33 @@ if (isset($_SESSION['nombreUsuario']) && $_SESSION['idUsuario']){
 
         <!-- Template Main JS File -->
         <script src="./assets/js/main.js"></script>
+        
+        <script>
+        function openDiveditar(idPrestamo, clienteNA, cobradorNA, monto, tasa, cuotas) {
+            let get = document.querySelector('#editar');
+            get.style.display = 'block';
+            
+            var idPrestamoEdit = document.getElementById("idPrestamoEdit");
+            var clienteEdit = document.getElementById("clienteEdit");
+            var cobradorEdit = document.getElementById("cobradorEdit");
+            var montoEdit = document.getElementById("montoEdit");
+            var tasaEdit = document.getElementById("tasaEdit");
+            var cuotasEdit = document.getElementById("cuotasEdit");
+
+            //Asignar valores a los campos del pop-up
+
+            idPrestamoEdit.value = idPrestamo;
+            clienteEdit.value = clienteNA;
+            cobradorEdit.value = cobradorNA;
+            montoEdit.value = monto;
+            tasaEdit.value = tasa;
+            cuotasEdit.value = cuotas;
+            
+            console.log('id Prestamo:', idPrestamo);
+            console.log('clienteNA:', clienteNA);
+            console.log('cobradorNA:', cobradorNA);
+        }
+        </script>
 
     </body>
 </html>
